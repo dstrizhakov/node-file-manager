274305102c73a2235d0479fd08a3aa6ac87e9e63
Dmitry Strizhakov <d.strizhakov@vk.team>
2024-01-30 15:53:28 +0300

    feat: get user name and solution ls

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import {readdir, stat, cp} from 'node:fs/promises';
 import path from 'node:path';
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
 import {fileURLToPath} from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -7,42 +9,44 @@ const __dirname = path.dirname(__filename);
 
 
 class FileManager {
-    constructor() {
+    constructor(user) {
         this.currentDirectory = __dirname;
-        this.user = 'anonimous'
+        this.user = user || 'anonymous';
         this.systemUser = ''
     }
 
     start() {
-        process.stdin.on('data', (data) => {
-            switch (data.toString().trim()) {
+        process.argv.forEach((arg) => {
+            if (arg.startsWith('--username'))
+                this.user = arg.split('=')[1];
+        })
+        const rl = readline.createInterface({ input, output });
+        rl.write(`Welcome to the File Manager, ${this.user}!\n`)
+        rl.on('line', async (line) => {
+            const command = line.toString().trim().split(' ')[0];
+            const params = line.toString().trim().split(' ').slice(1);
+            switch (command) {
                 case 'ls' :
-                    this.ls();
+                    await this.ls();
                     break;
             }
         })
     }
 
-    async ls(){
+    async ls() {
         try {
             const items = await readdir(this.currentDirectory);
-            const listTable = await this.getlistTable(items);
+            const listTable = [];
+            for (let i = 0; i < items.length; i++) {
+                const itemStat = await stat(path.join(this.currentDirectory, items[i]));
+                listTable.push({Name: items[i], Type: itemStat.isDirectory() ? 'directory' : 'file'})
+            }
             console.table(listTable);
         } catch (error) {
             throw Error('FS operation failed')
         }
     }
-
-    async getlistTable (items) {
-        const result = [];
-        for(let i = 0; i < items.length; i++) {
-            const itemStat = await stat(path.join(this.currentDirectory, items[i]));
-            result.push({Name: items[i], Type: itemStat.isDirectory() ? 'directory':'file'})
-        }
-        return result;
-    }
 }
 
 const fileManager = new FileManager();
-
 fileManager.start()
